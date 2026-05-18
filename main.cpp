232df9f25d7932555b698f8de47ff173bc1db912
YueC <Roxiyetto@163.com>
2026-05-18 12:01:17 +0800

    深度缓存

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ double TriangleArea(int x0,int y0,int x1,int y1,int x2,int y2)
     return (x0*(y1-y2)+x2*(y0-y1)+(x1*(y2-y0)))*0.5;
 }
 
-void Triangle(TGAImage &img, int x0, int y0, int x1, int y1, int x2, int y2, TGAColor color) {
+void Triangle(TGAImage &img, TGAImage &zbuffer,int x0, int y0,int z0, int x1, int y1, int z1,int x2, int y2,int z2, TGAColor color) {
     int boxMinX = std::min(x0, std::min(x1, x2));
     int boxMaxX = std::max(x0, std::max(x1, x2));
     int boxMinY = std::min(y0, std::min(y1, y2));
@@ -53,27 +53,31 @@ void Triangle(TGAImage &img, int x0, int y0, int x1, int y1, int x2, int y2, TGA
     #pragma omp parallel for
     for (int x=boxMinX; x<=boxMaxX; x++) {
         for (int y=boxMinY; y<=boxMaxY; y++) {
-
             double alpha = TriangleArea(x, y, x1, y1, x2, y2) / triangleArea;
             double beta = TriangleArea(x, y, x2, y2, x0, y0) / triangleArea;
             double gamma = TriangleArea(x, y, x0, y0, x1, y1) / triangleArea;
+            uint8_t z = (int)(alpha*z0 + beta*z1 + gamma*z2);
+            if (alpha<0 || beta<0 || gamma<0) continue;
+            if (z <= zbuffer.get(x, y)[0]) continue;
+            zbuffer.set(x, y,{z});
+            img.set(x, y, color);
 
-            if (alpha >= 0 && beta >= 0 && gamma >= 0) {
-                img.set(x, y, color);
-            }
         }
     }
 }
 
 
-std::tuple<int,int> project(Vec3f v) {
+std::tuple<int,int,int> project(Vec3f v) {
     return { (v.x + 1.) *  width/2,
-             (v.y + 1.) * height/2 };
+             (v.y + 1.) * height/2 ,
+                (v.z + 1.) * 255/2
+    };
 }
 int main() {
 
 
     TGAImage img(width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
     std::string path;
     std::cout << "Enter model path:";
@@ -87,16 +91,17 @@ int main() {
         Vec3f v1 = model.vert(face[1]);
         Vec3f v2 = model.vert(face[2]);
 
-        auto [x0, y0] = project(v0);
-        auto [x1, y1] = project(v1);
-        auto [x2, y2] = project(v2);
+        auto [x0, y0, z0] = project(v0);
+        auto [x1, y1, z1] = project(v1);
+        auto [x2, y2, z2] = project(v2);
 
         TGAColor rnd;
         for (int c=0; c<3; c++) rnd[c] = std::rand()%255;
 
-        Triangle(img, x0, y0, x1, y1, x2, y2,rnd);
+        Triangle(img,zbuffer, x0, y0,z0, x1, y1, z1,x2, y2,z2,rnd);
     }
 
     img.write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
     return 0;
 }
